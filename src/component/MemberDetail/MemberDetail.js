3f59257b8884515a4160740440d3617ca8362123
Agry88 <rgok307085@gmail.com>
2022-06-17 00:46:12 +0800

    Add Basic Content --Agry

--- src/component/MemberDetail/MemberDetail.js
+++ src/component/MemberDetail/MemberDetail.js
@@ -1,9 +1,20 @@
 import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
 
 function MemberDetail() {
-    return ( 
-        <Box>MemberDetail</Box>
-     );
+    return (
+        <Box sx={{p:5}}>
+            <Stack direction={"row"}> {/* 切成左右邊 左邊顯示可更改內容 右邊顯示頭像 */}
+                <Stack sx={{width:"100rem"}}> {/* 左邊區塊 顯示可更改內容 */}
+                    Put content here
+                    <div>another content</div> 
+                </Stack>
+                <Stack> {/* 左邊區塊 顯示頭像 */}
+                    Put UserIcon Here
+                </Stack>
+            </Stack>
+        </Box>
+    );
 }
 
 export default MemberDetail;
